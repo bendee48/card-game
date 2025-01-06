f21323c4973119f3b0abc17347743f51206c7c9f
bendee <benbanj@hotmail.com>
2025-01-06 01:10:10 +0000

    Adding a shuffle function
    - Adding a shuffle function that reorders the cards, to be hooked up to clicking a card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -33,11 +33,28 @@ function Cards() {
     return list; // Wait for all fetches to complete and return the data
   }
 
+  // Fisher Yates algo O(n) to shuffle the cards
+  function shuffle() {
+    let cardsCopy = [...cards];
+
+    for (let i = cardsCopy.length -1; i >= 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [cardsCopy[i], cardsCopy[j]] = [cardsCopy[j], cardsCopy[i]];
+    }
+
+    setCards(cardsCopy);
+  }
+
 
   return (
     <>
       <h1>Cards</h1>
-      {cards.map(card => <div key={card.name} className="card"><img src={card.url} alt={card.name} /></div>)}
+      {
+        cards.map(card => 
+          <div key={card.name} className="card" onClick={shuffle}>
+            <img src={card.url} alt={card.name} />
+          </div>)
+      }
     </>
   )
 }
